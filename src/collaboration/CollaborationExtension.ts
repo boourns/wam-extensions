14757bf0b6a9dd533d697426314a4096e8b0eab0
Tom Burns <tom@burns.ca>
2022-03-23 14:41:55 -0400

    proxy yjs edit types, allow multiple operations to apply in transaction

--- src/collaboration/CollaborationExtension.ts
+++ src/collaboration/CollaborationExtension.ts
@@ -1,12 +1,20 @@
 // Collaboration Extension lets plugins manage collaboratively edited text documents.
 
+export type CollaborationOperation = {
+    operation: "INSERT" | "DELETE"
+    position: number
+    length?: number
+    text?: string
+}
+
 export interface CollaborationDocumentInterface {
     toString(): string
-    onUpdate(callback: (update: any) => void): void
+    onUpdate(callback?: (operations: CollaborationOperation[]) => void): void
     insert(position: number, text: string): void
     delete(index: number, length: number): void
+    applyOperations(operations: CollaborationOperation[]): void
 }
 
 export class CollaborationExtension {
-    getDocument?: (pluginId: string, documentId: string) => CollaborationDocumentInterface
+    getDocument?: (pluginId: string, documentId: string) => Promise<CollaborationDocumentInterface>
 }
