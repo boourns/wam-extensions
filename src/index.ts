f3566f5d2acea4924bbc8a0ce5e013ce859bbb7b
Tom Burns <tom@burns.ca>
2022-04-07 23:06:27 -0400

    .js needed

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
-import {WAMExtensions} from "./WAMExtensions"
+import {WAMExtensions} from "./WAMExtensions.js"
 export {WAMExtensions}
 
-import {AssetExtension, AssetExtensionInterface, WamAsset} from "./assets/AssetExtension"
+import {AssetExtension, AssetExtensionInterface, WamAsset} from "./assets/AssetExtension.js"
 export {AssetExtension, AssetExtensionInterface, WamAsset} 
 
-import {NoteExtension,NoteDefinition, NoteListenerCallback} from "./notes/NoteExtension"
+import {NoteExtension,NoteDefinition, NoteListenerCallback} from "./notes/NoteExtension.js"
 export {NoteExtension,NoteDefinition, NoteListenerCallback}
 
-import {PatternExtension, PatternDelegate, PatternEntry} from "./patterns/PatternExtension"
+import {PatternExtension, PatternDelegate, PatternEntry} from "./patterns/PatternExtension.js"
 export {PatternExtension, PatternDelegate, PatternEntry}
 
-import {ModulationTargetExtension, ModulationTargetDelegate} from "./modulationTarget/ModulationTargetExtension"
+import {ModulationTargetExtension, ModulationTargetDelegate} from "./modulationTarget/ModulationTargetExtension.js"
 export {ModulationTargetExtension, ModulationTargetDelegate}
 
-import {VideoExtension, VideoExtensionDelegate, VideoExtensionOptions, VideoModuleConfig} from "./video/VideoExtension"
+import {VideoExtension, VideoExtensionDelegate, VideoExtensionOptions, VideoModuleConfig} from "./video/VideoExtension.js"
 export {VideoExtension, VideoExtensionDelegate, VideoExtensionOptions, VideoModuleConfig}
 
-import { CollaborationExtension, CollaborationDocumentInterface, CollaborationOperation, RemoteSelection } from "./collaboration/CollaborationExtension"
+import { CollaborationExtension, CollaborationDocumentInterface, CollaborationOperation, RemoteSelection } from "./collaboration/CollaborationExtension.js"
 export { CollaborationExtension, CollaborationDocumentInterface, CollaborationOperation, RemoteSelection }
 
-import { RecordingExtension, RecordingDelegate } from "./recording/RecordingExtension"
+import { RecordingExtension, RecordingDelegate } from "./recording/RecordingExtension.js"
 export { RecordingExtension, RecordingDelegate }
 
 export default {
